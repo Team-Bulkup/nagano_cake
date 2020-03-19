2e5ada600ca90571da60a7834ed2bff950b967ba
sotto97 <sotto.bw97@gmail.com>
2020-03-19 06:44:44 +0000

    [add] category_id カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_080216) do
+ActiveRecord::Schema.define(version: 2020_03_19_064311) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_080216) do
     t.boolean "is_available", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
