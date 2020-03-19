42e16cdd63a06690c7293c5b65a088b2271bb739
takatibi <taka123nyw@gmail.com>
2020-03-19 10:03:00 +0000

    [update]ヘッダー、フッター、トップ画面の作成。[add]トップ画面のケーキの写真追加。homesのviews追加。Categorysのviews追加。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,14 @@
 class HomesController < ApplicationController
-  def top
-    
-  end
+ 
 end
+
+
+
+# home.top.html.erbの記述。
+# Home.top
+
+# <% @products.each do|product| %>
+# 		<%= product.image %>
+# 		<%= product.name %>
+# 		<%= product.no_tax_price %>
+# 	<% end %>
