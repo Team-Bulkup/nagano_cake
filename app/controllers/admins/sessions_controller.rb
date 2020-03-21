8b61d0b58599a5475c0cb84284240b01aea91309
sotto97 <sotto.bw97@gmail.com>
2020-03-21 05:36:19 +0000

    [update] adminのログイン画面作成/admin_controllerでadminのpath設定/seedに管理者情報記載

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -24,4 +24,17 @@ class Admins::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed In successfully"
+    new_admin_product_path #後にadminのtopの画面にpathするように変更。（to 仲井）
+  end
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Signed Up successfully"
+    new_admin_product_path #後にadminのtopの画面にpathするように変更。（to 仲井）
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 end
