0dc0e740ba3896636cfb3ad4c4c4aae2fc61d67e
teru75 <love.akg.75@gmail.com>
2020-03-21 08:57:53 +0000

    [update]配送先一覧＆登録画面　[add]配送先登録機能

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,8 +1,19 @@
 class AddressesController < ApplicationController
   def index
+    @customer = Customer.find(current_customer.id)
+    @address = Address.new
+
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash[:notice] = "配送先を登録しました"
+      redirect_to addresses_path
+    else
+      render :index
+    end
   end
 
   def destroy
@@ -12,5 +23,10 @@ class AddressesController < ApplicationController
   end
 
   def update
+  end
+
+  protected
+  def address_params
+    params.require(:address).permit(:postcode, :address, :name, :phone_number)
   end
 end
