a5643b7c7e41b3226dc4beeafd33b0aebed1a7e0
sotto97 <sotto.bw97@gmail.com>
2020-03-22 11:57:39 +0000

    [update] 管理者商品機能画面 カテゴリーのindexの表示変更

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,19 +1,40 @@
 class Admin::ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @products = Product.all
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "Product was successfully added."
+      redirect_to admin_product_path(@product)
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path, notice: "Product was successfully updated."
+    else
+      render "edit"
+    end
+  end
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :detail, :category_id, :no_tax_price, :is_available)
   end
 end
