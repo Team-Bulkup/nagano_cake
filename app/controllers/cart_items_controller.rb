7be033d87e6ed3c4a2f58eb8f7cf6d7573f51ea5
takatibi <taka123nyw@gmail.com>
2020-03-22 11:08:40 +0000

    [update]cart-itemのコントローラー編集。index追加

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,16 +1,29 @@
 class CartItemsController < ApplicationController
   def index
+   @products = Product.all
   end
 
   def create
+  cart = CartItems.New
+  cart.save
+  redirect_to new_order_path
   end
 
   def destroy
+    @products = CartItems.find(params[:id])
+    @products.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
   end
 
   def update
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:image, :name ,:no_tax_price)
   end
 end
