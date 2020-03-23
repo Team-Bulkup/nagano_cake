af0c62ddbc4c5aa156d8c3587220ecd21cd7ef8c
sotto97 <sotto.bw97@gmail.com>
2020-03-23 09:19:16 +0000

    [update] バリテージョンの設定と商品一覧画面に新規商品登録のボタン作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ApplicationRecord
   belongs_to :category
 
   attachment :image
+
+	validates :image, presence: true
+	validates :name, presence: true
+	validates :detail, presence: true
+	validates :category_id, presence: true
+	validates :no_tax_price, presence: true
+	validates :is_available, presence: true
+
 end
