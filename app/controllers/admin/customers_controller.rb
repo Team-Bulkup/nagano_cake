ff7388d851524de0b65094634545d9c069e50283
teru75 <love.akg.75@gmail.com>
2020-03-24 07:47:39 +0000

    [update]会員編集画面＆編集機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -9,8 +9,21 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報を編集しました。"
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+
+  protected
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postcode, :address, :phone_number, :is_deleted)
   end
 end
