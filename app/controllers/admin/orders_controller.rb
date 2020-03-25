324f6a0a534a0e5fc9abacb8352c8729883ca50a
teru75 <love.akg.75@gmail.com>
2020-03-25 06:43:45 +0000

    [update]注文一覧画面　[add]会員の注文一覧画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,10 @@
 class Admin::OrdersController < ApplicationController
   def index
+      @orders = Order.all
+  end
+
+  def customer_index
+    @customer = Customer.find(params[:customer_id])
   end
 
   def show
