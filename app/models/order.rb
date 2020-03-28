642f6b104e50a9a027bafd789782dfd89aed0569
takatibi <taka123nyw@gmail.com>
2020-03-28 09:48:00 +0000

    [update]cart-itemのindexの商品続けるボタンのリンク作変更。ステータスの表示切り替え

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products, dependent: :destroy
+  enum status:{waitmoney:0, moneycheck:1, making:2, sentready:3, finishsenting:4}
 end
