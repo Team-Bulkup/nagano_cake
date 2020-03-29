bc3c2b3f1f8dd4055981e0ba56fbc14904e15998
teru75 <love.akg.75@gmail.com>
2020-03-29 09:10:35 +0000

    [update]注文ステータスの自動更新

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -3,15 +3,21 @@ class Admin::OrderProductsController < ApplicationController
   def update
     @order_product = OrderProduct.find(params[:id])
     @order_product_status = params[:order_product][:status]
-        @order_product.update(status: @order_product_status)
-
-        if @order_product_status == "making"
-            @order_product.order.update(status: "making")
-        elsif @order_product_status == "finishmaking"
-            @order_product.order.update(status: "sentready")
-        end
-      redirect_to admin_order_path(@order_product.order.id)
+    @order_product.update(status: @order_product_status)
+    @order_product.order.order_products.each.with_index(1) do |order_product, i|
+      unless order_product.status == "finishmaking"
+        break
+      end
+      if i == @order_product.order.order_products.length
+        @order_product.order.update(status: "sentready")
+      end
+    end
+    if @order_product_status == "making"
+        @order_product.order.update(status: "making")
+    end
+    redirect_to admin_order_path(@order_product.order.id)
   end
+
   protected
   def order_product_params
     params.require(:order_product).permit(:status)
